fc75648372a25c6897f3e45f2db930088a735f27
s-shreya01 <sinhakishanvi09@gmail.com>
2024-10-09 17:01:42 +0530

    Add files via upload
    this project enhance my skill and knowledge

---
+++ converting temperature program.py
@@ -0,0 +1,43 @@
+# TEMPERATURE CONVERTER PROGRAM
+
+    # CELSIUS
+# Celsius to Fahrenheit
+celsius = float(input("\nenter the temperature in celsius:"))
+fahrenheit = (celsius*1.8)+32
+print("temperature in fahrenheit is",fahrenheit)
+
+# Celsius to Kelvin
+celsius = float(input("\nenter the temperature in celsius:"))
+kelvin = celsius + 273.15
+print("temperature in kelvin is",kelvin)
+
+
+
+
+   # FAHRENHEIT
+# Fahrenheit to Celsius
+fahrenheit = float(input("\nenter the temperature in fahrenheit:"))
+celsius = (fahrenheit-32)+5/9
+print("temperature in celsius is",celsius)
+
+# Fahrenheit to Kelvin
+fahrenheit = float(input("\nenter the temperature in fahrenheit:"))
+kelvin = (fahrenheit + 459.67)/1.8
+print("temperature in kelvin is",kelvin)
+
+
+
+   # KELVIN
+# Kelvin to Fahrenheit
+kelvin = float(input("\nenter the temperature in kelvin"))
+fahrenheit = (1.8*kelvin)-459.67
+print("temperature in fahrenheit is",fahrenheit)
+
+# Kelvin to Celsius
+kelvin = float(input("\nenter the temperature in kelvin:"))
+celsius = kelvin - 273.15
+print("temperature in celsius is",celsius)
+
+
+
+
